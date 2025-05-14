069065c967b4eb0acc3a4af51bc3a1dfc25e9d5c
himanshu <himanshu@gmail.com>
2025-05-14 22:55:14 +0530

    inital commit

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+//import '../App.css'
+import CarRentalSearch from "./CarRentalSearch.jsx";
+import HowItWorks from "./HowItWorks.jsx";
+import Features from "./Features.jsx";
+import RentalDeals from "./RentalDeals.jsx";
+import TestimonialsSection from "./TestimonialsSection.jsx";
+function App() {
+  return (
+    <>
+      <div>
+        <CarRentalSearch />
+        <RentalDeals />
+        <HowItWorks />
+        <Features />
+        <TestimonialsSection />
+      </div>
+    </>
+  );
+}
+
+export default App;
